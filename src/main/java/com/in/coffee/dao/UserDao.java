e38010dd7d1580041674afaf9be91faa6b7cd85d
manle423 <trungmanle.vm@gmail.com>
2023-12-05 16:06:21 +0700

    create coffee management project

---
+++ src/main/java/com/in/coffee/dao/UserDao.java
@@ -0,0 +1,9 @@
+package com.in.coffee.dao;
+
+import com.in.coffee.POJO.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+public interface UserDao extends JpaRepository<User, Integer> {
+    User findByEmailId(@Param("email") String email);
+}
---
+++ src/main/java/com/in/coffee/restImpl/UserRestImpl.java
@@ -0,0 +1,30 @@
+package com.in.coffee.restImpl;
+
+import com.in.coffee.constants.CafeConstants;
+import com.in.coffee.rest.UserRest;
+import com.in.coffee.service.UserService;
+import com.in.coffee.utils.CafeUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class UserRestImpl implements UserRest {
+
+    @Autowired
+    UserService userService;
+
+    @Override
+    public ResponseEntity<String> signUp(Map<String, String> requestMap) {
+        try{
+            return userService.signUp(requestMap);
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return CafeUtils.getResponseEntity(CafeConstants.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
